c12ae1323309b297f85c315a98af06330debcb0b
jasonbringle <jasonbringle@gmail.com>
2020-06-14 18:38:59 -0700

    added login page and is sending token to localstorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import BubblePage from './components/BubblePage';
+import PrivateRoute from './components/PrivateRoute'
+
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -9,6 +12,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
+        <PrivateRoute path='private-route' component={BubblePage} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import axiosWithAuth from '../util/axiosWithAuth';
+import { useHistory } from 'react-router-dom';
 
 const Login = () => {
+  const [ values, setValues ] = useState({
+    username:'',
+    password: ''
+  })
+
+  const { push } = useHistory()
+
+  const handleChange = e => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const submitForm = e => {
+    e.preventDefault();
+    axiosWithAuth()
+    .post("/login", values)
+    .then(res => {
+      localStorage.setItem("token", res.data.payload);
+      push("/private-route")})
+    .catch(err => console.log(err.message, err.response))
+
+  }
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <form onSubmit={submitForm}>
+        <input type='text' name='username' placeholder='User Name' value={values.username} onChange={handleChange}/>
+        <input type='text' name='password' placeholder='Password' value={values.password} onChange={handleChange}/>
+        <button >Login</button>
+      </form>
     </>
   );
 };
